37d06bf817d921f6c917953bc5b5ec89a36f5fcd
Harsh Mangal <harshmangal2005@gmail.com>
2026-03-30 23:52:49 +0530

    Singleton

--- Singleton.cpp
+++ Singleton.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 class Singleton{
 
-    public:
+
+    private:
+   static Singleton* instance;//c++ me yahan directly nahi kar sakte
     Singleton(){
         cout<<"Singleton constructor called New Object Created"<<endl;
     }
 
+    public:
+    static Singleton* getInstance(){//static class ko belong karega isko call karne ke liye sirf class ka name naki object ka nam
+        
+            if(instance==nullptr)
+           instance= new Singleton();
+    
+    return instance;
+
+};
 };
 
+Singleton* Singleton::instance=nullptr;
+
 int main(){
-    Singleton* s1=new Singleton();
-    Singleton* s2=new Singleton();
+    Singleton* s1= Singleton::getInstance();
+    Singleton* s2= Singleton::getInstance();
     cout<<(s1==s2)<<endl;
 }
+
+//multithreading me problem hoga its not thread safe .. lock use karna hoga .. critical section 
